62c15591a0719f85be9eedf45aed1f52f05b7993
Leu Family <romanianfobjr@gmail.com>
2021-08-03 15:27:15 -0500

    successfully pulling and displaying information from github

--- web-module-project-lifecycle/src/App.js
+++ web-module-project-lifecycle/src/App.js
@@ -11,6 +11,18 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/D2MBingo5`)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          ...this.state,
+          userInfo: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
 render(){
   return (
     <div className="App">
@@ -20,7 +32,10 @@ render(){
         <button>Fetch User</button>
       </form>
       <div id='gitUser'>
-
+        <h2>{this.state.userInfo.login} | {this.state.userInfo.name}</h2>
+        <img width={200} src={this.state.userInfo.avatar_url} alt='user avatar'/>
+        <p>{this.state.userInfo.bio}</p>
+        <p>{this.state.userInfo.location}</p>
       </div>
     </div>
   );
