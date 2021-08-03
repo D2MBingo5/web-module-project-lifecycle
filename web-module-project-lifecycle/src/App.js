9dff200e86f280d051ed1bb9b0d3995d2b1c1aab
Leu Family <romanianfobjr@gmail.com>
2021-08-03 15:41:44 -0500

    mvp complete

--- web-module-project-lifecycle/src/App.js
+++ web-module-project-lifecycle/src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     super();
     this.state = {
       userInfo: [],
-      username: ''
+      userFollowers: [],
+      username: '' // stretch
     }
   }
 
@@ -21,21 +22,42 @@ class App extends React.Component {
         })
       })
       .catch(err => console.log(err))
+    axios.get(`https://api.github.com/users/D2MBingo5/followers`)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          ...this.state,
+          userFollowers: res.data
+        })
+      })
+      .catch(err => console.log(err))
   }
 
 render(){
   return (
     <div className="App">
       <h1>Github User Information</h1>
-      <form>
+      {/* <form>
         <input />
         <button>Fetch User</button>
-      </form>
+      </form> */}
       <div id='gitUser'>
         <h2>{this.state.userInfo.login} | {this.state.userInfo.name}</h2>
         <img width={200} src={this.state.userInfo.avatar_url} alt='user avatar'/>
         <p>{this.state.userInfo.bio}</p>
         <p>{this.state.userInfo.location}</p>
+      </div>
+      <div id='gitFollowers'>Followers:
+        {
+          this.state.userFollowers.map(flwr => {
+            return(
+              <div>
+               <img width={50} src={flwr.avatar_url} alt='follower profile' /> 
+               <span>{flwr.login}</span>
+              </div>              
+            )
+          })
+        }
       </div>
     </div>
   );
